c22a5528462a55f441c5b8e32b19f570ddbe50d1
AlexSosnkowski <alexsosnkowski@gmail.com>
2021-12-15 22:01:37 +0000

    final push

--- src/submissions/homework_final.lean
+++ src/submissions/homework_final.lean
@@ -1,5 +1,5 @@
 import tactic.ring 
-
+--Alexander Sosnkowski 
 /-
 Read, understand (collaborating if necessary) the material
 in chapter 17 of Jeremy Avigad's *Logic and Proof.* It's here:
@@ -13,15 +13,16 @@ the chapter.
 Write the principle of complete induction using the notation of symbolic logic. 
 "Let P be any property that satisfies the following: for any natural number n, whenever P holds of every number less than n, 
 it also holds of n. Then P holds of every natural number." --original from textbook
-∀ (n m any : ℕ), ∀ (P : ℕ → Prop), ∀ (m < n), P m → P n → P any
 
-For all n of type nat, if for every m of type nat less than n some property p holds and if p holds for n, then p holds for all natural numbers
+def completeInduction := ∀ (n m any : ℕ), ∀ (P : ℕ → Prop), ∀ (m < n), P m → P n → P any
+
 
 #2. Solve at least one of #2 and #3. Give
     detailed but informal proofs. 
     #2 Show that for every n, 0^2+1^2+2^2+…n^2 = (1/6)n(1+n)(1+2n).
-
-    In the default case when n equals zero, 0^2 = 0 = (1/6)*0*1*1
+    This can be proven by induction, therefore we must show the base case of n = 0 and 
+    that our formula holding for n implies that it holds for n + 1. 
+    In the default case when n equals zero, 0^2 = 0 = (1/6)*0*1*1. This is fairly trivial and is easily proven with basic arithmetic and reflexivity.
     Therefore, we can continue with the base case proven and we create the inductive hypothesis that 0^2+1^2+2^2+…n^2 = (1/6)n(1+n)(1+2n)
     is true for n. All that is left to be shown is that the propery holds for n + 1.
     Thus, it is sufficent to show :  0^2 + 1^2 + 2^2 + ...... n^2 + (n+1)^2 = (1/6)(n+1)(1+n+1)(1+2(n+1))
@@ -37,6 +38,7 @@ For all n of type nat, if for every m of type nat less than n some property p ho
     which is true by the reflexive property of equality - QED 
 
     #3 Show that for every n, 0^3+1^3+…+n^3 = (1/4)n^2(n+1)^2.
+    A very similar proof would follow as in the above case, a formal proof is given below. 
 -/
  def completeInduction := ∀ (n m any : ℕ), ∀ (P : ℕ → Prop), ∀ (m < n), P m → P n → P any
 
@@ -134,6 +136,47 @@ To test out of the final exam ...
 --#3 (Extra Credit): #5 or #9
   --  #5 Given the definition of the Fibonacci numbers in Section 17.3, prove Cassini’s identity:
     -- for every n, F^2 n+1 − Fn+2 Fn=(−1)^n. Hint: in the induction step, write F^2 n+2 as Fn+2(Fn+1+Fn).
+    def sum_to_4 : ℕ → ℕ 
+      | (nat.zero)    := nat.zero           -- base case
+      | (n' + 1)  := (sum_to_3 n') + (n' + 1) * (n' + 1) * (n' + 1)
+    
+    
+    def myFib : ℕ → ℕ 
+      | (nat.zero) := 0 
+      | (1) := 1
+      | ((n+2)) := myFib(n+1) + myFib(n)
+ 
+
+    #eval myFib 0
+    #eval myFib 1
+    #eval myFib 2
+    #eval myFib 3
+    #eval myFib 4
+
+    example : ∀ (n : ℕ), myFib (n + 1) * myFib (n+1) - myFib (n+2) * myFib (n) = 1 := --the problem comes here with exponentiation 
+    begin
+
+    end
+
+    /-
+    Do to problems with defining exponentiation and transfering from natural numbers to integers,
+    I have provided an informal proof below.
+    
+     It is to be shown that F^2 n+1 − Fn+2 Fn =(−1)^n by induction. (Where F represents our Fibonacci function)
+     In the base case of n = 0 we find that F^2 1 - F2 * F0 = (-1)^0, which simplifies to 1 - 0 = (-1)^0, which in turn is 1 = 1. 
+     Thus, we may continue and assume an inductive hypothesis that F^2 n+1 − Fn+2 Fn =(−1)^n for n. What remains to be shown is that
+     this holds for n+1, therefore we must prove that F^2 (n+1+1) - F(n+1+2) * F(n+1) = (-1)^(n+1) 
+     Since we know that F(n+2) = F(n+1) + F(n) by our defenition of the Fibonacci numbers, we can subsitute it in finding that,
+     (F(n+1) + F(n))*(F(n+1) + F(n)) - F(n+2 +1) * F(n+1) = (-1)^n * (-1) 
+     (F(n+1) + F(n))*(F(n+1) + F(n)) - (F(n+1) + F(n) + F(n+1)) * F(n+1) = (-1)^n * (-1) 
+     F^2(n+1) + 2F(n) + F^2(n) - F^2(n+1) - F(n)*F(n+1) - F^2(n+1) = (-1)^n * (-1)
+     Now we subsitute the hypothesis 
+     F^2(n+1) + 2F(n) + F^2(n) - F^2(n+1) - F(n)*F(n+1) - F^2(n+1) = (F^2 n+1 − Fn+2 Fn) * (-1)
+     Through expansion and algebraic simplification we recieve 0 = 0 and by reflexivity we know this to be true. - QED
+     
+
+    -/
+    
 
     --#9 Let V be a non-empty set of integers such that the following two properties hold:
 
